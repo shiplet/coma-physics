090529464df1d1cf829e24f868b3da7e0e9adcb4
Ship <mnshiplet@gmail.com>
2020-05-13 18:43:27 -0600

    update particle definition & tests

--- lib/classes/Particle.ts
+++ lib/classes/Particle.ts
@@ -26,18 +26,32 @@ export class Particle {
 	damping: number
 
 	/**
+	 * Construct a new particle
+	 * @param p {Vector3} position
+	 * @param v {Vector3} velocity
+	 * @param a {Vector3} acceleration
+	 * @param d {number} damping
+	 */
+	constructor(p: Vector3, v: Vector3, a: Vector3, d: number) {
+		this.position = p
+		this.velocity = v
+		this.acceleration = a
+		this.damping = d
+	}
+
+	/**
 	 * The numerical mass of the particle
 	 */
 	setMass(m: number) {
 		if (m > 0) {
 			this.inverseMass = 1 / m
 		} else {
-			throw new Error('mass must be a positive, non-zero value')
+			throw new Error(`mass must be a positive, non-zero value, got: ${m}`)
 		}
 	}
 
 	/**
-	 * Useful in determining acceleration
+	 * Useful in determining acceleration and avoiding infinite masses
 	 */
 	protected inverseMass: number
 
@@ -45,7 +59,31 @@ export class Particle {
 		if (m > 0) {
 			this.inverseMass = 1 / m
 		} else {
-			throw new Error('mass must be a positive, non-zero value')
+			throw new Error(`mass must be a positive, non-zero value, got: ${m}`)
 		}
+	}
+
+	integrate(duration: number) {
+		if (this.inverseMass <= 0) {
+			return
+		}
+
+		if (duration <= 0) {
+			throw new Error(
+				`duration must be a positive, non-zero value, got: ${duration}`,
+			)
+		}
+
+		this.position.addScaledVectorUpdate(this.velocity, duration)
+
+		const resultingAcc = this.acceleration
+
+		this.velocity.addScaledVectorUpdate(resultingAcc, duration)
+
+		const dampingOffset = Math.pow(this.damping, duration)
+
+		this.velocity.componentProductUpdate(
+			new Vector3(dampingOffset, dampingOffset, dampingOffset),
+		)
 	}
 }
--- tests/Particle.spec.ts
+++ tests/Particle.spec.ts
@@ -1,6 +1,12 @@
 import { Particle } from '../lib/classes/Particle'
+import { Vector3 } from '../lib/classes/Vector3'
 
 test('Particle instantiates', () => {
-	const p = new Particle()
+	const position = new Vector3(1, 2, 3)
+	const velocity = new Vector3(1, -1, 2)
+	const acceleration = new Vector3(0, 1, -1)
+	const damping = 0.999
+
+	const p = new Particle(position, velocity, acceleration, damping)
 	expect(p).toBeInstanceOf(Particle)
 })
